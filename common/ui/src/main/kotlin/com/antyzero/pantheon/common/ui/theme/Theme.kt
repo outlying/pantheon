589e2190a3b03773eeff7980935ab78ce6f90ead
outlying <outlying@gmail.com>
2024-04-16 00:13:48 +0200

    Minor changes

--- common/ui/src/main/kotlin/com/antyzero/pantheon/common/ui/theme/Theme.kt
+++ common/ui/src/main/kotlin/com/antyzero/pantheon/common/ui/theme/Theme.kt
@@ -63,9 +63,9 @@ fun PantheonApplicationTheme(
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
+    val context = LocalContext.current
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
 
